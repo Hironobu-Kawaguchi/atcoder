a3bf60340acc188d408719765a318a2ac7ec4115
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-08-10 17:48:08 +0900

    DP by numpy

--- abc015_4_DP.py
+++ abc015_4_DP.py
@@ -2,47 +2,27 @@
 
 import numpy as np
 
-Wmax = int(input())
-n, k = map(int,input().split())
+W = int(input())
+N, K = map(int,input().split())
 
-dp = np.zeros((k+1, Wmax+1), np.int)
+dp = np.zeros((K+1, W+1), np.int)
 
-for i in range(n):
-  w, v = map(int,input().split())
-  if w <= Wmax:
-    dp[1:, w:] = np.maximum(dp[1:,w:], dp[:k,:Wmax+1-w]+v)
-print(dp[k, Wmax])
+for i in range(N):
+  A, B = map(int,input().split())
+  if A <= W:
+    dp[1:, A:] = np.maximum(dp[1:,A:], dp[:K,:W+1-A]+B)
+print(dp[K, W])
 
 
-
-# # import numpy as np
-# MAX_N = 50
-# MAX_U = 10000
-# done = [[0] * (MAX_U + 1) for _ in range(MAX_N)]
-# memo = [[0] * (MAX_U + 1) for _ in range(MAX_N)]
+# import numpy as np
 
 # W = int(input())
-# N, K = map(int, input().split())
-# A, B = [], []
+# N, K = map(int,input().split())
+
+# dp = np.zeros((K+1, W+1), np.int)
+
 # for i in range(N):
-#     a, b = map(int, input().split())
-#     A.append(a)
-#     B.append(b)
-
-# # i番目以降の画像で、幅の総和がu以下、画像の枚数がj以下となるように選ぶ
-# def search(i, j, u):
-#     if i == N:
-#         return 0
-#     if done[i][u]:
-#         return memo[i][u]
-#     if u < A[i] or j == K:
-#         res = search(i+1, j, u)
-#     else:
-#         res = max(search(i+1, j, u), search(i+1, j+1, u-A[i]) + B[i])
-#     done[i][u] = 1
-#     memo[i][u] = res
-#     return res
-
-# ans = search(0, 0, W)
-
-# print(ans)
+#   A, B = map(int,input().split())
+#   if A <= W:
+#     dp[1:, A:] = np.maximum(dp[1:,A:], dp[:K,:W+1-A]+B)
+# print(dp[K, W])
