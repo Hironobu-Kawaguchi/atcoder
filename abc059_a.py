aacb1c3927605830a15207e07d004ba5cf58bb3c
Hironobu Kawaguchi <40334053+Hironobu-Kawaguchi@users.noreply.github.com>
2019-08-13 17:14:02 +0900

    20190813 practice

---
+++ abc059_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc059/tasks/abc059_a
+
+s1, s2, s3 = input().split()
+ans = s1[0].upper() + s2[0].upper() + s3[0].upper()
+print(ans)
---
+++ abc059_b.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc059/tasks/abc059_b
+
+A = int(input())
+B = int(input())
+if A > B:
+    print("GREATER")
+elif A < B:
+    print("LESS")
+else:
+    print("EQUAL")
+    
