bb9f7668529f36ed0386644686ab668b9bdb686a
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-09-15 23:59:30 +0900

    20190915 practice

--- abc141_e_TLE.py
+++ abc141_e_TLE.py
@@ -1,15 +1,35 @@
 # https://atcoder.jp/contests/abc141/tasks/abc141_e
+
 # TLE
 N = int(input())
 S = input()
+DP = [[0]*5005 for _ in range(5005)]
 
+for i in range(N):
+    for j in range(N):
+        if S[i] == S[j]:
+            DP[N-i][N-j] = DP[N-i+1][N-j+1] + 1
 ans = 0
-for i in range(N-1):
-    for j in range(i+1+ans, N-ans):
-        for l in range(ans+1, min(j-i, N-j)+1):
-            if S[i:i+l] == S[j:j+l]:
-                ans = max(ans, l)
-            else:
-                break
+for i in range(N):
+    for j in range(N):
+        if i >= j:
+            continue
+        now = min(DP[i][j], j-i)
+        ans = max(ans, now)
 
 print(ans)
+
+# # TLE
+# N = int(input())
+# S = input()
+
+# ans = 0
+# for i in range(N-1):
+#     for j in range(i+1+ans, N-ans):
+#         for l in range(ans+1, min(j-i, N-j)+1):
+#             if S[i:i+l] == S[j:j+l]:
+#                 ans = max(ans, l)
+#             else:
+#                 break
+
+# print(ans)
