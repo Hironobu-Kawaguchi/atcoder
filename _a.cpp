ee324fd8f4758b5da6dade9a67fd2fb7b08a1b2c
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-09-22 19:42:24 +0900

    define all(v)

--- _a.cpp
+++ _a.cpp
@@ -19,6 +19,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(v) (v).begin(),(v).end()
 typedef pair<int, int> P;
 typedef long long ll;
 const int INF = 1001001001;
