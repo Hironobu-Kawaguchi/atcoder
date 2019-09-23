ce1f6dbf7b811a897d272cb6f61c5e88387e8110
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-09-23 23:05:58 +0900

    20190923 ruby practice

---
+++ abc141_a.rb
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc141/tasks/abc141_a
+
+S = gets.chomp
+
+if S == 'Sunny'
+  puts('Cloudy')
+elsif S == 'Cloudy'
+  puts('Rainy')
+elsif S == 'Rainy'
+  puts('Sunny')
+end
