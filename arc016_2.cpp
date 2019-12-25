e22d2172e6d0ac2c58b00ef845a2de84bfbc0ccf
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2019-12-25 22:16:02 +0900

    practice

---
+++ arc016_2.cpp
@@ -0,0 +1,43 @@
+// https://atcoder.jp/contests/arc016/tasks/arc016_2
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(v) (v).begin(),(v).end()
+#define chmin(x,y) x = min(x,y)
+#define chmax(x,y) x = max(x,y)
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+	int N;
+	cin >> N;
+    vector<string> x(N);
+    rep(i,N) cin >> x[i];
+    int ans = 0;
+    rep(i,N) rep(j,9) {
+        if (x[i][j] == 'x') ans += 1;
+        if (x[i][j] == 'o' and (i == 0 or  x[i][j] != x[i-1][j])) ans += 1;
+    }
+	cout << ans << endl;
+	return 0;
+}
