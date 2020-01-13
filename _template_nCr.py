1469775eeddb7715db7e9673210ac4283e136a13
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-01-13 23:11:31 +0900

    practice

--- _template_nCr.py
+++ _template_nCr.py
@@ -8,3 +8,11 @@ def nCr(n, r, mod=10**9+7):
         numer = numer * (n+1-i) % mod
         denom = denom * i % mod
     return numer * pow(denom, mod-2, mod) % mod
+
+MOD = 10**9+7
+MAXN = 10**5
+f = [1]
+for i in range(MAXN):
+    f.append(f[-1] * (i+1) % MOD)
+def nCr(n, r, mod=MOD):
+    return f[n] * pow(f[r], mod-2, mod) * pow(f[n-r], mod-2, mod) % mod
--- abc151_d_bfs.py
+++ abc151_d_bfs.py
@@ -1,31 +1,32 @@
 # https://atcoder.jp/contests/abc151/tasks/abc151_d
 
-INF = 1001001001
 from collections import deque
-h,w = map(int,input().split())
-s = [input() for _ in range(h)]
-di = (-1, 0, 1, 0)
-dj = ( 0,-1, 0, 1)
+INF = 1001001001
+di = [-1, 0, 1, 0]
+dj = [ 0,-1, 0, 1]
+
+H, W = map(int, input().split())
+S = [input() for _ in range(H)]
 
 ans = 0
-for si in range(h):
-    for sj in range(w):
-        if s[si][sj] == '#': continue
-        dist = [[INF] * w for _ in range(h)]
+for si in range(H):
+    for sj in range(W):
+        if S[si][sj] == '#': continue
+        dist = [[INF] * W for _ in range(H)]
         dist[si][sj] = 0
-        q = deque([(si,sj)])
-        while len(q) > 0:
-            i,j = q.popleft()
+        q = deque([(si, sj)])
+        while q:
+            i, j = q.popleft()
             for dir in range(4):
                 ni = i + di[dir]
                 nj = j + dj[dir]
-                if (ni<0 or ni>=h or nj<0 or nj>=w): continue
-                if s[ni][nj] == '#': continue
+                if (ni<0 or ni>=H or nj<0 or nj>=W): continue
+                if S[ni][nj] == '#': continue
                 if dist[ni][nj] != INF: continue
-                q.append((ni,nj))
                 dist[ni][nj] = dist[i][j] + 1
-        for i in range(h):
-            for j in range(w):
+                q.append([ni, nj])
+        for i in range(H):
+            for j in range(W):
                 if dist[i][j] == INF: continue
                 ans = max(ans, dist[i][j])
 print(ans)
