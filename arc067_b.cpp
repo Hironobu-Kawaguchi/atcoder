ae2fa52a2bd39a8161727bbf23621d483bee7719
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-01-26 19:45:49 +0900

    practice

---
+++ arc067_b.cpp
@@ -0,0 +1,42 @@
+// https://atcoder.jp/contests/arc067/tasks/arc067_b
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define all(v) (v).begin(),(v).end()
+#define chmin(x,y) x = min(x,y)
+#define chmax(x,y) x = max(x,y)
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
+int main() {
+	ll n, a, b;;
+	cin >> n >> a >> b;
+    vector<ll> X(n);
+    rep(i,n) cin >> X[i];
+    ll ans = 0;
+    rep(i,n-1) ans += min((X[i+1]-X[i])*a, b);
+	cout << ans << endl;
+	return 0;
+}
