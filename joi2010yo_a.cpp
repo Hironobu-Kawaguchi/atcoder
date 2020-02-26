c027f5fd5d0eb512388e85aeceafd3e146436a03
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-02-26 20:54:37 +0900

    practice

---
+++ joi2010yo_a.cpp
@@ -0,0 +1,44 @@
+// https://atcoder.jp/contests/joi2010yo/tasks/joi2010yo_a
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+	int ans;
+	cin >> ans;
+    int x;
+    rep(i,9) {
+        cin >> x;
+        ans -= x;
+    }
+	cout << ans << endl;
+	return 0;
+}
