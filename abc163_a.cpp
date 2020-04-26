312bbf5b5f48fdfccba4adcd616e9fc3b5aa6567
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-04-26 14:27:15 +0900

    practice

---
+++ abc163_a.cpp
@@ -0,0 +1,42 @@
+// https://atcoder.jp/contests/abc163/tasks/abc163_a
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	int r;
+	cin >> r;
+    float ans = M_PI * 2 * r;
+	cout << ans << "\n";
+	return 0;
+}
