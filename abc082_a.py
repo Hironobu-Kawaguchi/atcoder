bb6b342b2a4a3013890a3eedb724dfe9aa84c99c
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-05-03 20:31:21 +0900

    practice

---
+++ abc082_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc082/tasks/abc082_a
+
+a, b = map(int, input().split())
+ans = (a+b+1)//2
+print(ans)
---
+++ abc165_a.cpp
@@ -0,0 +1,48 @@
+// https://atcoder.jp/contests/abc165/tasks/abc165_a
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	int k, a, b;
+	cin >> k >> a >> b;
+    string ans = "NG";
+    for (int i = a; i <= b; i++) {
+        if (i%k==0) {
+            ans = "OK";
+            break;
+        }
+    }
+	cout << ans << "\n";
+	return 0;
+}
