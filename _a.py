bbf2eface96f9aba9dd0dcc911de27353a4442b1
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-06-14 20:48:02 +0900

    add numba

--- _a.py
+++ _a.py
@@ -3,7 +3,16 @@ import sys
 # def input(): return sys.stdin.readline().rstrip()
 # input = sys.stdin.readline
 input = sys.stdin.buffer.readline
+from numba import njit
+from functools import lru_cache
 sys.setrecursionlimit(10 ** 7)
+
+@njit('(i8,i8[::1],i4[::1])', cache=True)
+def main():
+    @lru_cache(None)
+    def dfs():
+        return
+    return
 
 S = input()
 n = int(input())
