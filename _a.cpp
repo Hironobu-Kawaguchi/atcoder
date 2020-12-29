ee99d62fdbd3f7d93dd2bdc571150e706985ef69
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-12-29 12:58:50 +0900

    add ac-library

--- _a.cpp
+++ _a.cpp
@@ -18,6 +18,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define drep(i,n) for(int i = (n-1); i >= 0; i--)
 #define all(v) (v).begin(),(v).end()
