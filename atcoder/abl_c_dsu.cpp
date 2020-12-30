58f269cd877344a036fac3255eaffaa41ff43785
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2020-12-30 16:33:33 +0900

    practice ACL

---
+++ atcoder/abl_c_dsu.cpp
@@ -0,0 +1,35 @@
+// https://atcoder.jp/contests/abl/tasks/abl_c
+#include<bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	int n, m;
+	cin >> n >> m;
+    dsu d(n);
+    rep(i,m) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        d.merge(a,b);
+    }
+	cout << d.groups().size() - 1 << "\n";
+	return 0;
+}
