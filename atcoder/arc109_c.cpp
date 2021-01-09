984dd7ef59d4dad80c7ca4ca474a5c92f479c26c
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-01-09 20:33:34 +0900

    practice

---
+++ atcoder/arc109_c.cpp
@@ -0,0 +1,55 @@
+// https://atcoder.jp/contests/arc109/tasks/arc109_c
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    char win[222][222];
+    win['R']['R'] = win['R']['S'] = win['S']['R'] = 'R';
+    win['S']['S'] = win['S']['P'] = win['P']['S'] = 'S';
+    win['P']['P'] = win['P']['R'] = win['R']['P'] = 'P';    
+	int n, k;
+	cin >> n >> k;
+    string s;
+    cin >> s;
+    while(k--) {
+        string t = s+s;
+        rep(i,n) s[i] = win[t[i*2]][t[i*2+1]];
+    }
+	cout << s[0] << "\n";
+	return 0;
+}
