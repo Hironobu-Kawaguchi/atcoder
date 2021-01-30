cd5d38c04bd6e3eeb724fb2495c40c82b6e8b229
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-01-30 20:57:57 +0900

    practice

---
+++ atcoder/code_festival_qualB_a.py
@@ -0,0 +1,28 @@
+# https://atcoder.jp/contests/code-festival-2014-qualb/tasks/code_festival_qualB_a
+# # def input(): return sys.stdin.readline().rstrip()
+# # input = sys.stdin.readline
+# from numba import njit
+# from functools import lru_cache
+
+# import sys
+# input = sys.stdin.buffer.readline
+# sys.setrecursionlimit(10 ** 7)
+
+a, b = map(int, input().split())
+print(max(a,b))
+
+
+# S = input()
+# n = int(input())
+# N, K = map(int, input().split())
+# l = list(map(int, (input().split())))
+# A = [[int(i) for i in input().split()] for _ in range(N)]
+
+# @njit('(i8,i8[::1],i4[::1])', cache=True)
+# def main():
+#     @lru_cache(None)
+#     def dfs():
+#         return
+#     return
+
+# main()
