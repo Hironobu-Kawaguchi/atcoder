3cf428946080686989e225717ed075c787924d8a
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-02-13 23:42:00 +0900

    simple solution

--- atcoder/arc112_b.py
+++ atcoder/arc112_b.py
@@ -1,18 +1,28 @@
 # https://atcoder.jp/contests/arc112/tasks/arc112_b
 
 B, C = map(int, input().split())
-ans = 0
-ans += max(C-2,0)//2 + (C//2) + 1
-if C>=1:
-    ans += max(C-1,0)//2 + max(C-1,0)//2 + 1
-if B>=0 and (max((C-1),0)//2-B)>=0:
-    ans -= (max(C-1,0)//2-B) - (B-C//2) + 1
-elif B<0 and (max((C-2),0)//2+B)>=0:
-    ans -= (max(C-2,0)//2+B) - (-B-(max(C-1,0)//2)) + 1
-ans = max(ans, max(C-2,0)//2 + (C//2) + 1)  # そのままで最大
-if B==0:
-    ans = max(ans, max(C-1,0)//2 + (C//2) + 1)  # そのままで最大
+x1 = -B - (C-1)//2
+x2 = -B + (C-1)//2
+x3 =  B - C//2
+x4 =  B + max(0,(C-2))//2
+ans = max(x4,x2) - min(x1,x3) + 1
+if x3>x2:
+    ans -= x3 - x2 - 1
 print(ans)
+
+# B, C = map(int, input().split())
+# ans = 0
+# ans += max(C-2,0)//2 + (C//2) + 1
+# if C>=1:
+#     ans += max(C-1,0)//2 + max(C-1,0)//2 + 1
+# if B>=0 and (max((C-1),0)//2-B)>=0:
+#     ans -= (max(C-1,0)//2-B) - (B-C//2) + 1
+# elif B<0 and (max((C-2),0)//2+B)>=0:
+#     ans -= (max(C-2,0)//2+B) - (-B-(max(C-1,0)//2)) + 1
+# ans = max(ans, max(C-2,0)//2 + (C//2) + 1)  # そのままで最大
+# if B==0:
+#     ans = max(ans, max(C-1,0)//2 + (C//2) + 1)  # そのままで最大
+# print(ans)
 
 
 # import sys
