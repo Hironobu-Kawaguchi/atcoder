34fd7d29866d3775ccfeba904cc7d59b9b50d517
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-05-16 13:45:22 +0900

    practice

---
+++ atcoder/abc201_e.cpp
@@ -0,0 +1,81 @@
+// https://atcoder.jp/contests/abc201/tasks/abc201_e
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+using mint = modint1000000007;
+
+struct Edge {
+    int to;
+    ll w;
+    Edge(int to=0, ll w=0) : to(to), w(w) {}
+};
+
+int n;
+vector<vector<Edge>> g;
+
+int dfs(int v, int x=0, int p=-1) {
+    int res = x;
+    for (auto e : g[v]) {
+        if (e.to==p) continue;
+        res += dfs(e.to, x^(e.w&1), v);
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	cin >> n;
+    g.resize(n);
+    rep(i,n-1) {
+        int u, v;
+        ll w;
+        cin >> u >> v >> w;
+        --u, --v;
+        g[u].emplace_back(v, w);
+        g[v].emplace_back(u, w);
+    }
+
+    mint ans = 0, two = 1;
+    rep(i, 60) {
+        int x = dfs(0);
+        ans += mint(n-x)*x*two;
+        rep(v, n) rep(j, g[v].size()) g[v][j].w >>= 1;
+        two *= 2;
+    }
+	cout << ans.val() << "\n";
+	return 0;
+}
