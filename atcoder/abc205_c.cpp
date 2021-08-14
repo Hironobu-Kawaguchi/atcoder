6810c86a95c757c715b35929b63f6c558b9b3eec
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-08-14 20:45:54 +0900

    ABC205 virtual contest

---
+++ atcoder/abc205_c.cpp
@@ -0,0 +1,51 @@
+// https://atcoder.jp/contests/abc205/tasks/abc205_c
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	int a, b, c;
+	cin >> a >> b >> c;
+    if (c%2==0) {
+        a = abs(a);
+        b = abs(b);
+    }
+    if (a==b)     cout << "=" << endl;
+    else if (a<b) cout << "<" << endl;
+    else          cout << ">" << endl;
+	return 0;
+}
---
+++ atcoder/abc205_d.cpp
@@ -0,0 +1,61 @@
+// https://atcoder.jp/contests/abc205/tasks/abc205_d
+#include<iostream>
+// #include<algorithm>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+	int n, q;
+	cin >> n >> q;
+    vector<ll> a(n+1);
+    rep(i,n) cin >> a[i+1];
+    sort(a.begin(), a.end());
+    rep(i,q) {
+        ll k;
+        cin >> k;
+        int l = 0, r = n;
+        while (l+1<r) {
+            int now = (l+r)/2;
+            if (k>a[now]-now) l = now;
+            else r = now;
+        }
+        ll ans;
+        if (k>a[r]-r) ans = k + r;
+        else          ans = k + l;
+    	cout << ans << "\n";
+    }
+	return 0;
+}
