2fc8e7aa7deeb72b264346ccad1099377809bfd1
Hironobu-Kawaguchi <hironobukawaguchi3@gmail.com>
2021-08-29 23:16:04 +0900

    next(it)

--- a/_a.py
+++ a/_a.py
@@ -4,9 +4,9 @@
 # from numba import njit
 # from functools import lru_cache
 
-import sys
-input = sys.stdin.buffer.readline
-sys.setrecursionlimit(10 ** 7)
+# import sys
+# input = sys.stdin.buffer.readline
+# sys.setrecursionlimit(10 ** 7)
 
 
 
@@ -15,6 +15,10 @@ sys.setrecursionlimit(10 ** 7)
 # N, K = map(int, input().split())
 # l = list(map(int, (input().split())))
 # A = [[int(i) for i in input().split()] for _ in range(N)]
+
+# import sys
+# it = map(int, sys.stdin.buffer.read().split())
+# N = next(it)
 
 # @njit('(i8,i8[::1],i4[::1])', cache=True)
 # def main():
