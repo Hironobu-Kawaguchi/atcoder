a57a17fbf673f44cd04ca928e7725fc4da578b75
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2022-05-14 20:56:02 +0900

    practice

--- atcoder/abc221_b.py
+++ atcoder/abc221_b.py
@@ -9,25 +9,38 @@
 # sys.setrecursionlimit(10 ** 7)
 # MOD = 998244353
 
-S = input()
-T = input()
+S = list(input())
+T = list(input())
 
-def main():
-    ans = 'Yes'
-    if S==T:
-        print(ans)
-        return
-    chk = []
-    for i in range(len(S)):
-        if S[i]!=T[i]:
-            chk.append(i)
-    if len(chk)==2 and chk[0]+1==chk[1] and S[chk[0]]==T[chk[1]] and S[chk[1]]==T[chk[0]]:
-        print(ans)
-    else:
-        print('No')
-    return
+ans = "No"
+if S==T: ans = "Yes"
+for i in range(len(S)-1):
+    S[i], S[i+1] = S[i+1], S[i]
+    if S==T: ans = "Yes"
+    S[i], S[i+1] = S[i+1], S[i]
 
-main()
+print(ans)
+
+
+# S = input()
+# T = input()
+
+# def main():
+#     ans = 'Yes'
+#     if S==T:
+#         print(ans)
+#         return
+#     chk = []
+#     for i in range(len(S)):
+#         if S[i]!=T[i]:
+#             chk.append(i)
+#     if len(chk)==2 and chk[0]+1==chk[1] and S[chk[0]]==T[chk[1]] and S[chk[1]]==T[chk[0]]:
+#         print(ans)
+#     else:
+#         print('No')
+#     return
+
+# main()
 
 # S = input()
 # n = int(input())
