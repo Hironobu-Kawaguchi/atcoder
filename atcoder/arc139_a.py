8c8c1983baeef04cb93e08e05e2fe1fb29c422ca
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2022-05-29 20:36:28 +0900

    practice

---
+++ atcoder/arc139_a.py
@@ -0,0 +1,38 @@
+# https://atcoder.jp/contests/arc139/tasks/arc139_a
+# # def input(): return sys.stdin.readline().rstrip()
+# # input = sys.stdin.readline
+# from numba import njit
+# from functools import lru_cache
+
+import sys
+input = sys.stdin.buffer.readline
+# sys.setrecursionlimit(10 ** 7)
+
+N = int(input())
+T = list(map(int, (input().split())))
+ans = 0
+for i in range(N):
+    tmp = 1<<T[i]
+    ans = (ans//tmp + 1) * tmp
+    if ans%(tmp*2)==0:
+        ans += tmp
+print(ans)
+
+# S = input()
+# n = int(input())
+# N, K = map(int, input().split())
+# l = list(map(int, (input().split())))
+# A = [[int(i) for i in input().split()] for _ in range(N)]
+
+# import sys
+# it = map(int, sys.stdin.buffer.read().split())
+# N = next(it)
+
+# @njit('(i8,i8[::1],i4[::1])', cache=True)
+# def main():
+#     @lru_cache(None)
+#     def dfs():
+#         return
+#     return
+
+# main()
