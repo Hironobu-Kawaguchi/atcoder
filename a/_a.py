b9d5baf3da6321523c111f6d8dd70e0064d3c773
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2023-01-15 20:49:59 +0900

    template

--- a/_a.py
+++ a/_a.py
@@ -1,11 +1,10 @@
 # 
-# # def input(): return sys.stdin.readline().rstrip()
-# # input = sys.stdin.readline
 # from numba import njit
 # from functools import lru_cache
 
 import sys
 input = sys.stdin.buffer.readline
+# def input(): return sys.stdin.readline().rstrip()
 # sys.setrecursionlimit(10 ** 7)
 
 
