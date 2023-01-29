0b4e46ccf9f855827c428841442be82597d2c69a
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2023-01-29 15:21:10 +0900

    review

--- atcoder/arc141_a.py
+++ atcoder/arc141_a.py
@@ -1,49 +1,44 @@
 # https://atcoder.jp/contests/arc141/tasks/arc141_a
-# # def input(): return sys.stdin.readline().rstrip()
-# # input = sys.stdin.readline
-# from numba import njit
-# from functools import lru_cache
+
+def main():
+    S = input()
+    N = int(S)
+    k = len(S)
+    cand = [10**(k-1)-1, 11]
+    for a in range(1, k//2+1):
+        tmp = int(S[:a]*(k//a))
+        if tmp<=N:
+            cand.append(tmp)
+        cand.append(int(str((int(S[:a])-1))*(k//a)))
+    cand.sort(reverse=True)
+    # print(cand)
+    print(cand[0])
+
+T = int(input())
+for ti in range(T):
+    main()
+
 
 # import sys
 # input = sys.stdin.buffer.readline
 # sys.setrecursionlimit(10 ** 7)
-
-def main():
-    S = input()
-    candidates = []
-    for i in range(1, len(S)):
-        if len(S)%i: continue
-        candidate = S[:i] * (len(S)//i)
-        if int(candidate)<=int(S): candidates.append(int(candidate))
-        if int(S[:i])>=2 and len(S[:i])==len(str(int(S[:i])-1)):
-            candidate = (str(int(S[:i])-1)) * (len(S)//i)
-            if int(candidate)<=int(S): candidates.append(int(candidate))
-    candidates.sort(reverse=True)
-    # print(candidates)
-    if len(candidates)==0:
-        return '9'*(len(S)-1)
-    return candidates[0]
-
-T = int(input())
-for ti in range(T):
-    print(main())
-
-
-# S = input()
-# n = int(input())
-# N, K = map(int, input().split())
-# l = list(map(int, (input().split())))
-# A = [[int(i) for i in input().split()] for _ in range(N)]
-
-# import sys
-# it = map(int, sys.stdin.buffer.read().split())
-# N = next(it)
-
-# @njit('(i8,i8[::1],i4[::1])', cache=True)
 # def main():
-#     @lru_cache(None)
-#     def dfs():
-#         return
-#     return
+#     S = input()
+#     candidates = []
+#     for i in range(1, len(S)):
+#         if len(S)%i: continue
+#         candidate = S[:i] * (len(S)//i)
+#         if int(candidate)<=int(S): candidates.append(int(candidate))
+#         if int(S[:i])>=2 and len(S[:i])==len(str(int(S[:i])-1)):
+#             candidate = (str(int(S[:i])-1)) * (len(S)//i)
+#             if int(candidate)<=int(S): candidates.append(int(candidate))
+#     candidates.sort(reverse=True)
+#     # print(candidates)
+#     if len(candidates)==0:
+#         return '9'*(len(S)-1)
+#     return candidates[0]
 
-# main()
+# T = int(input())
+# for ti in range(T):
+#     print(main())
+
