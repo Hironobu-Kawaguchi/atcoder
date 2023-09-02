2419851100d148c930a1073009c1bc0f7c5e5a43
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2023-09-02 18:35:17 +0900

    ABC273

---
+++ atcoder/abc273_b.cpp
@@ -0,0 +1,57 @@
+// https://atcoder.jp/contests/newjudge-2308-algorithm/tasks/abc273_b
+#include<iostream>
+// #include<algorithm>
+// #include<cmath>
+// #include <ctime>
+// #include<string>
+// #include<numeric>
+// #include<vector>
+// #include<map>
+// #include<tuple>
+// #include<set>
+// #include<queue>
+// #include<deque>
+// #include<regex>
+// #include<bitset>
+// #include<iomanip>
+// #include<complex>
+// #include<stack>
+// #include<functional>
+
+#include<bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define drep(i,n) for(int i = (n-1); i >= 0; i--)
+#define all(v) (v).begin(),(v).end()
+#define maxs(x,y) (x = max(x,y))
+#define mins(x,y) (x = min(x,y))
+template <class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+template <class T> T lcm(T a, T b) { return a/gcd(a,b)*b; }
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001002003004005006ll;
+const ll MOD = 1e9+7;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll x;
+    ll k;
+    cin >> x >> k;
+    ll pow10 = 1;
+    rep(i,k) {
+        x /= pow10;
+        int m = x%10;
+        if (m<5) x -= m;
+        else x += 10-m;
+        x *= pow10;
+        pow10 *= 10;
+    }
+    cout << x << endl;
+	return 0;
+}
--- atcoder/abc273_b.py
+++ atcoder/abc273_b.py
@@ -1,34 +1,29 @@
-# https://atcoder.jp/contests/abc273/tasks/abc273_b
-# # def input(): return sys.stdin.readline().rstrip()
-# # input = sys.stdin.readline
+# https://atcoder.jp/contests/newjudge-2308-algorithm/tasks/abc273_b
 # from numba import njit
 # from functools import lru_cache
 
 import sys
 input = sys.stdin.buffer.readline
-sys.setrecursionlimit(10 ** 7)
-
-def f(x, i):
-    tmp = 10**i
-    left = x // (tmp*10)
-    right = x % (tmp*10)
-    if right>=5*tmp:
-        y = (left+1) * tmp * 10
-    else:
-        y = left * tmp *10
-    return y
+# def input(): return sys.stdin.readline().rstrip()
+# sys.setrecursionlimit(10 ** 7)
 
 X, K = map(int, input().split())
+pow10 = 1
 for i in range(K):
-    X = f(X, i)
+    X //= pow10
+    m = X % 10
+    if m < 5: X -= m
+    else: X += 10 - m
+    X *= pow10
+    pow10 *= 10
 print(X)
 
 
 
 # S = input()
-# n = int(input())
+# N = int(input())
 # N, K = map(int, input().split())
-# l = list(map(int, (input().split())))
+# A = list(map(int, (input().split())))
 # A = [[int(i) for i in input().split()] for _ in range(N)]
 
 # import sys
