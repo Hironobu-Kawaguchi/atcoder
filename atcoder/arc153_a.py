249335af14df6cfb788afd62fe48cea125544550
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2023-11-07 23:36:26 +0900

    update

--- atcoder/arc153_a.py
+++ atcoder/arc153_a.py
@@ -4,36 +4,23 @@
 # from numba import njit
 # from functools import lru_cache
 
-import sys
-input = sys.stdin.buffer.readline
-# sys.setrecursionlimit(10 ** 7)
-
 N = int(input())
-N -= 1
-lst = [0] * 6
-for i in range(6):
-    N, tmp = divmod(N, 10)
-    lst[i] = tmp
-lst.reverse()
-# print(lst)
-ans = (lst[0]+1)*110000000 + lst[1]*1000000 + lst[2]*100000 + lst[3]*11000 + lst[4]*101 + lst[5]*10
+S = str(N + 99999)
+ans = S[0] + S[0] + S[1:4] + S[3] + S[4:] + S[4]
 print(ans)
 
-# S = input()
-# N = int(input())
-# N, K = map(int, input().split())
-# A = list(map(int, (input().split())))
-# A = [[int(i) for i in input().split()] for _ in range(N)]
 
 # import sys
-# it = map(int, sys.stdin.buffer.read().split())
-# N = next(it)
+# input = sys.stdin.buffer.readline
+# # sys.setrecursionlimit(10 ** 7)
 
-# @njit('(i8,i8[::1],i4[::1])', cache=True)
-# def main():
-#     @lru_cache(None)
-#     def dfs():
-#         return
-#     return
-
-# main()
+# N = int(input())
+# N -= 1
+# lst = [0] * 6
+# for i in range(6):
+#     N, tmp = divmod(N, 10)
+#     lst[i] = tmp
+# lst.reverse()
+# # print(lst)
+# ans = (lst[0]+1)*110000000 + lst[1]*1000000 + lst[2]*100000 + lst[3]*11000 + lst[4]*101 + lst[5]*10
+# print(ans)
--- atcoder/arc158_b.py
+++ atcoder/arc158_b.py
@@ -2,59 +2,71 @@
 # from numba import njit
 # from functools import lru_cache
 
-import sys
-input = sys.stdin.buffer.readline
-# def input(): return sys.stdin.readline().rstrip()
-# sys.setrecursionlimit(10 ** 7)
 import bisect
 from itertools import permutations
-INF = 1001001001.0
 
-def main():
-    N = int(input())
-    x = list(map(int, (input().split())))
-    x.sort()
-    st = set([0,1, N-2, N-1])
-    zero_idx = bisect.bisect_left(x, 0)
-    for i in range(2):
-        if zero_idx - 1 - i<0: break
-        st.add(zero_idx - 1 - i)
-    for i in range(2):
-        if zero_idx + i>N-1: break
-        st.add(zero_idx + i)
-    # print(st)
-    mx = -INF
-    mn = INF
-    for i in range(N):
-        for j, k in permutations(st, 2):
-            if i==j or i==k: continue
-            # print(i, j, k)
-            tmp = (x[i]+x[j]+x[k])/(x[i]*x[j]*x[k])
-            mx = max(mx, tmp)
-            mn = min(mn, tmp)
-    print(mn)
-    print(mx)
-    return
+N = int(input())
+X = list(map(int, input().split()))
+X.sort()
+idx_set = set()
+for i in range(3):
+    idx_set.add(i)
+for i in range(N-3, N):
+    idx_set.add(i)
+zero_idx = bisect.bisect_left(X, 0)
+for i in range(3):
+    if zero_idx + i >= N:
+        break
+    idx_set.add(zero_idx + i)
+for i in range(3):
+    if zero_idx - 1 - i < 0:
+        break
+    idx_set.add(zero_idx - 1 - i)
+# print(idx_set)
+ans_min, ans_max = 10**18, -10**18
 
+for i, j, k in permutations(idx_set, 3):
+    # print(i, j, k, (X[i] + X[j] + X[k])/(X[i] * X[j] * X[k]))
+    ans_min = min(ans_min, (X[i] + X[j] + X[k])/(X[i] * X[j] * X[k]))
+    ans_max = max(ans_max, (X[i] + X[j] + X[k])/(X[i] * X[j] * X[k]))
+print(ans_min)
+print(ans_max)
 
-main()
-
-
-# S = input()
-# N = int(input())
-# N, K = map(int, input().split())
-# A = list(map(int, (input().split())))
-# A = [[int(i) for i in input().split()] for _ in range(N)]
 
 # import sys
-# it = map(int, sys.stdin.buffer.read().split())
-# N = next(it)
+# input = sys.stdin.buffer.readline
+# # def input(): return sys.stdin.readline().rstrip()
+# # sys.setrecursionlimit(10 ** 7)
+# import bisect
+# from itertools import permutations
+# INF = 1001001001.0
 
-# @njit('(i8,i8[::1],i4[::1])', cache=True)
 # def main():
-#     @lru_cache(None)
-#     def dfs():
-#         return
+#     N = int(input())
+#     x = list(map(int, (input().split())))
+#     x.sort()
+#     st = set([0,1, N-2, N-1])
+#     zero_idx = bisect.bisect_left(x, 0)
+#     for i in range(2):
+#         if zero_idx - 1 - i<0: break
+#         st.add(zero_idx - 1 - i)
+#     for i in range(2):
+#         if zero_idx + i>N-1: break
+#         st.add(zero_idx + i)
+#     # print(st)
+#     mx = -INF
+#     mn = INF
+#     for i in range(N):
+#         for j, k in permutations(st, 2):
+#             if i==j or i==k: continue
+#             # print(i, j, k)
+#             tmp = (x[i]+x[j]+x[k])/(x[i]*x[j]*x[k])
+#             mx = max(mx, tmp)
+#             mn = min(mn, tmp)
+#     print(mn)
+#     print(mx)
 #     return
 
+
 # main()
+
