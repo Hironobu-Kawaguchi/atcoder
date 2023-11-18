1eeb4af75be59d2d4264466f6f3fd001a322b2fa
Hironobu Kawaguchi <hironobukawaguchi3@gmail.com>
2023-11-18 19:38:35 +0900

    typical

---
+++ typical90/typical90_ac_LazySegTree.py
@@ -0,0 +1,92 @@
+# https://atcoder.jp/contests/typical90/tasks/typical90_ac
+
+# https://qiita.com/ether2420/items/7b67b2b35ad5f441d686
+
+#####segfunc#####
+def segfunc(x, y):
+    return max(x,y)
+#################
+
+#####ide_ele#####
+ide_ele = 0
+#################
+
+class LazySegTree_RUQ:
+    def __init__(self,init_val,segfunc,ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1<<(n-1).bit_length()
+        self.tree = [ide_ele]*2*self.num
+        self.lazy = [None]*2*self.num
+        for i in range(n):
+            self.tree[self.num+i] = init_val[i]
+        for i in range(self.num-1,0,-1):
+            self.tree[i] = self.segfunc(self.tree[2*i],self.tree[2*i+1])
+    def gindex(self,l,r):
+        l += self.num
+        r += self.num
+        lm = l>>(l&-l).bit_length()
+        rm = r>>(r&-r).bit_length()
+        while r>l:
+            if l<=lm:
+                yield l
+            if r<=rm:
+                yield r
+            r >>= 1
+            l >>= 1
+        while l:
+            yield l
+            l >>= 1
+    def propagates(self,*ids):
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if v is None:
+                continue
+            self.lazy[i] = None
+            self.lazy[2*i] = v
+            self.lazy[2*i+1] = v
+            self.tree[2*i] = v
+            self.tree[2*i+1] = v
+    def update(self,l,r,x):
+        ids = self.gindex(l,r)
+        self.propagates(*self.gindex(l,r))
+        l += self.num
+        r += self.num
+        while l<r:
+            if l&1:
+                self.lazy[l] = x
+                self.tree[l] = x
+                l += 1
+            if r&1:
+                self.lazy[r-1] = x
+                self.tree[r-1] = x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.tree[i] = self.segfunc(self.tree[2*i], self.tree[2*i+1])
+    def query(self,l,r):
+        ids = self.gindex(l,r)
+        self.propagates(*self.gindex(l,r))
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l<r:
+            if l&1:
+                res = self.segfunc(res,self.tree[l])
+                l += 1
+            if r&1:
+                res = self.segfunc(res,self.tree[r-1])
+            l >>= 1
+            r >>= 1
+        return res
+
+
+W, N = map(int, input().split())
+seg = LazySegTree_RUQ([0]*W, segfunc, ide_ele)
+for i in range(N):
+    L, R = map(int, input().split())
+    L -= 1; R -= 1
+    new_h = seg.query(L, R+1) + 1
+    seg.update(L, R+1, new_h)
+    print(new_h)
